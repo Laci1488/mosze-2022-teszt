96934d9fb425dd41dd7e63ffee4222703e974d27
Laci1488 <73988245+Laci1488@users.noreply.github.com>
2022-09-10 15:14:55 +0200

    Update main.cpp
    Fellelt hibák kommentezése

--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];   //hibás megnevezés
+    std::cout << '1-100 ertekek duplazasa'   //stringeket ""-el kell megadni (esetleg endl a végére)
+    for (int i = 0;)   //hiányos feltétel
     {
-        b[i] = i * 2;
+        b[i] = i * 2;   //értékek kiírattatása lehet ebbe a ciklusba
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)   //felesleges for ciklus
     {
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++)   //hibás feltétel
     {
         atlag += b[i]
     }
